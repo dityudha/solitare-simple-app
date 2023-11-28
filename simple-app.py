f30c9a45ec8698e581e85911c0d0a34c592343da
dityudha <yudha14045@gmail.com>
2023-11-28 17:33:43 +0700

    push new

---
+++ simple-app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello ():
+  return 'Silahkan buka url di tab baru dan input /welcome/nama_anda untuk menampilkan pesan!'
+
+@app.route('/welcome/<name>')
+def welcome_name(name):
+  return 'welcome ' + name + '!'
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
