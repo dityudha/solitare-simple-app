f9042c08a9c6986cbade86784974e9166a90511f
dityudha <yudha14045@gmail.com>
2023-12-14 17:37:35 +0700

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello ():
-  return 'Silahkan buka url di tab baru dan input /welcome/nama_anda untuk menampilkan pesan!'
+  return 'Silahkan buka url di tab baru dan input /welcome/nama_anda untuk menampilkan pesan!!'
 
 @app.route('/welcome/<name>')
 def welcome_name(name):
